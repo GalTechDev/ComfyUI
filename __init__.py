9415ccae2cf8f3218955c120ed15bec2958ae12c
GalTechDev <poussigalitv@gmail.com>
2023-12-19 17:23:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,9 +5,12 @@ from typing import List
 from PIL import Image
 import io
 import base64
-import json
+import asyncio
+from time import sleep
 
 Lib = lib.App()
+
+save_path = ("sd_output", "output.png")
 
 url = "http://127.0.0.1:7860"
 
@@ -47,6 +50,7 @@ payload_json = {
         }
     },
     "prompt": "",
+    "negative_prompt": "",
     "steps": 5,
     "sampler_name": "DPM++ 2M Karras",
     "width" : 512,
@@ -54,7 +58,6 @@ payload_json = {
 }
 
 style = [
-    "base",
     "Modèle 3D",
     "Film Analogique",
     "Anime",
@@ -153,37 +156,100 @@ class ModelTransformer(discord.app_commands.Transformer):
 class StyleTransformer(discord.app_commands.Transformer):
     @classmethod
     async def transform(self, interaction: discord.Interaction, value: str):
-        pass
+        return value
+
+    @classmethod
+    async def autocomplete(self, interaction: discord.Interaction, value: str) -> List[discord.app_commands.Choice[str]]:
+        choices: List[discord.app_commands.Choice] = []
+        for m in style:
+            choices.append(discord.app_commands.Choice(name=m, value=m))
+        return choices[:25]
+
+class Prompt(discord.ui.Modal):
+    positive = discord.ui.TextInput(
+        label='Positive prompt',
+        required=False,
+        style=discord.TextStyle.long
+    )
+
+    negative = discord.ui.TextInput(
+        label='Negative prompt',
+        required=False,
+        style=discord.TextStyle.long,
+    )
+
+    def __init__(self, ctx, style, refiner, steps) -> None:
+        super().__init__(title='Prompt')
+        self.ctx = ctx
+        self.style = style
+        self.refiner = refiner
+        self.steps = steps
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f'Generating... 0%\n**{self.positive.value}**', ephemeral=True)
+        task = asyncio.create_task(gen_image(self.positive.value, self.negative.value, self.style, self.refiner, self.steps))
+
+        await update_progress(interaction, self.positive.value)
+        await task
+        await interaction.edit_original_response(content=f'**{self.positive.value}**', attachments=[discord.File(Lib.save.get_full_path(save_path[1],save_path[0]))])
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
 
 def get_model_list():
     response = requests.get(url=f'{url}/sdapi/v1/sd-models')
     r = response.json()
     return r
 
-def gen_image(prompt, steps=25):
+async def gen_image(p_prompt, n_prompt="", style="base", refiner="", steps=25):
     payload=payload_json
-    payload["prompt"] = prompt
+    payload["prompt"] = p_prompt
+    payload["negative_prompt"] = n_prompt
     payload["steps"] = steps
 
     response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+    
     if response.status_code == 200:
         r = response.json()
-
         image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
-        image.save('output.png')
+        image.save(Lib.save.get_full_path(save_path[1],save_path[0]))
+
     else:
         print(response.text)
 
+async def update_progress(ctx: discord.Interaction, prompt): #ctx: discord.Interaction, prompt
+    last_progress = 0.0
+    while True:
+        response = requests.get(url = "http://127.0.0.1:7860/sdapi/v1/progress")
+        r = response.json()
+        progress = r.get("progress")
+        print(f"progress : {int(progress*100)}%")
+        print(r)
+        if progress >= last_progress:
+            last_progress = progress
+            img_data = r.get("current_image")
+            if img_data:
+                image = Image.open(io.BytesIO(base64.b64decode(img_data)))
+                image.save(Lib.save.get_full_path(save_path[1],save_path[0]))
+                await ctx.edit_original_response(content=f"Generating... {progress*100}%\n**{prompt}**", attachments=[discord.File(Lib.save.get_full_path(save_path[1],save_path[0]))])
+            else:
+                await ctx.edit_original_response(content=f"Generating... {progress*100}%\n**{prompt}**")
+        else:
+            break
+        await asyncio.sleep(2)
 
 @Lib.event.event()
 async def on_ready():
     global models
     models = get_model_list()
+    if not Lib.save.existe(save_path[0]):
+        Lib.save.add_folder(save_path[0])
 
 @Lib.app.slash(name="text2image", description="génère une image par IA")
-async def text2image(ctx: discord.Interaction, style="", model: ModelTransformer=None, refiner: ModelTransformer=""):
-    pass
+async def text2image(ctx: discord.Interaction, style: StyleTransformer="base", refiner: ModelTransformer="", steps: int=25):
+    await ctx.response.send_modal(Prompt(ctx, style, refiner, steps))
 
 if __name__=="__main__":
     pass
     #gen_image("Dreamscape bird's eye view shot, by Marta Bevacqua, we see a beautiful birch forest and a lake, the lighting atmosphere is very beautiful, the light is very soft, everything is very bright, the weather is very cloudy, very mystical, it is rainy, we are in the north of Europe, The scene is reminiscent of a music video, in Vogue style, a very high quality photo shoot, full shot, art cinematic scene, --ar 2:3 --style raw --v 5.2")
+    #update_progress()
